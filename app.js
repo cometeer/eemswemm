c50e03409a9f437c66ec50dcb97e9f4bcbda037d
Tristan Geraets <tristang@protonmail.ch>
2016-04-23 13:20:50 +0200

    facebook 0.16

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+app.use(passport.initialize());
+app.use(passport.session());
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
