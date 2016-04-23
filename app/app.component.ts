201610350c6e5038980ce72d33201b7c56fe3840
Tristan Geraets <tristang@protonmail.ch>
2016-04-23 17:01:08 +0200

    facebook 0.27

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,17 @@
 /**
  * Created by tristang on 23/04/16.
  */
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {FBConnector} from 'ng2-facebook/ng2-facebook';
 
 @Component({
     selector: 'my-app',
     template: '<h1>Welcome to Eem Swem\'m</h1>'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+    ngOnInit() {
+        var fbCon:FBConnector = new FBConnector('1814502352104058');
+        fbCon.initFB();
+    }
+}
